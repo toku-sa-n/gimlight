b5c390592d0c4e3b429bf40d2289b2a287b1da33
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-03 11:15:39 +0900

    style: indentation

--- app/Dungeon/Predefined.hs
+++ app/Dungeon/Predefined.hs
@@ -8,17 +8,18 @@ import           Entity        (Entity)
 import           Map.Tile      (TileMap, allWallTiles, floorTile, wallTile)
 
 firstEventMap :: Entity -> Dungeon
-firstEventMap player = dungeon (stringArrayToMap [ "####################"
-                                            , "#..................#"
-                                            , "#..................#"
-                                            , "#..................#"
-                                            , "#..................#"
-                                            , "#..................#"
-                                            , "#..................#"
-                                            , "#..................#"
-                                            , "####################"
-                                            ])
-                [player]
+firstEventMap player = dungeon (stringArrayToMap
+    [ "####################"
+    , "#..................#"
+    , "#..................#"
+    , "#..................#"
+    , "#..................#"
+    , "#..................#"
+    , "#..................#"
+    , "#..................#"
+    , "####################"
+    ])
+    [player]
 
 stringArrayToMap :: [String] -> TileMap
 stringArrayToMap list = allWallTiles // [((x, y), tile c) | (y, row) <- zip [0..] list, (x, c) <- zip [0..] row]
