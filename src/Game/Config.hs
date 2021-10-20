eb366c441ee6e70ec741f5e929665c882c048728
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-20 22:43:40 +0900

    chore: define `readConfigOrDefault`

--- src/Game/Config.hs
+++ src/Game/Config.hs
@@ -1,16 +1,40 @@
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Game.Config
     ( Config
     , Language(..)
-    , initConfig
+    , readConfigOrDefault
     , getLocale
     ) where
+import           Data.Binary  (Binary, decodeFileOrFail, encodeFile)
+import           Data.Maybe   (fromMaybe)
+import           GHC.Generics (Generic)
 
-data Language = English | Japanese
+data Language = English | Japanese deriving (Generic)
+instance Binary Language
 
-newtype Config = Config { language :: Maybe Language }
+newtype Config = Config { language :: Maybe Language } deriving (Generic)
+instance Binary Config
+
+readConfigOrDefault :: IO Config
+readConfigOrDefault = fromMaybe initConfig <$> tryReadConfig
+
+tryReadConfig :: IO (Maybe Config)
+tryReadConfig = do
+    cfg <- decodeFileOrFail configFilePath
+
+    case cfg of
+        Right x -> return $ Just x
+        Left _  -> do
+            encodeFile configFilePath initConfig
+            return Nothing
 
 initConfig :: Config
 initConfig = Config { language = Nothing }
 
 getLocale :: Config -> Maybe Language
 getLocale Config { language = l } = l
+
+configFilePath :: FilePath
+configFilePath = "config"
