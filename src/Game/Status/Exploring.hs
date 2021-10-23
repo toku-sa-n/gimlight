c682959615ce62e133de119c5eb008d66173bdb3
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-23 11:36:47 +0900

    chore: define `ExploringHandler`

--- src/Game/Status/Exploring.hs
+++ src/Game/Status/Exploring.hs
@@ -1,3 +1,12 @@
 module Game.Status.Exploring
-    (
+    ( ExploringHandler
     ) where
+
+import           Dungeon (Dungeon)
+import           Log     (MessageLog)
+
+data ExploringHandler = ExploringHandler
+                      { currentDungeon :: Dungeon
+                      , otherDungeons  :: [Dungeon]
+                      , messageLog     :: MessageLog
+                      }
