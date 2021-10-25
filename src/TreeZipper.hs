927ce1ea058eaeff0fbf06ad1b476e4b2824a6c1
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-25 10:54:44 +0900

    chore: add `goUp`

--- src/TreeZipper.hs
+++ src/TreeZipper.hs
@@ -6,14 +6,18 @@ module TreeZipper
     , treeZipper
     , getFocused
     , modify
+    , goUp
     , goDownBy
     ) where
 
+import           Data.Binary   (Binary)
 import           Data.Foldable (find)
 import           Data.Tree     (Tree (Node, rootLabel, subForest))
 import           GHC.Generics  (Generic)
 
 data TreeCrumb a = TreeCrumb a [Tree a] deriving (Show, Ord, Eq, Generic)
+
+instance (Binary a) => Binary (TreeCrumb a)
 
 type TreeZipper a = (Tree a, [TreeCrumb a])
 
@@ -25,6 +29,11 @@ getFocused (t, _) = rootLabel t
 
 modify :: (a -> a) -> TreeZipper a -> TreeZipper a
 modify f (n@Node { rootLabel = r }, bs) = (n { rootLabel = f r }, bs)
+
+goUp :: TreeZipper a -> Maybe (TreeZipper a)
+goUp (Node { rootLabel = r, subForest = fs }, TreeCrumb newRootLabel newSubForest:bs) =
+    Just (Node { rootLabel = newRootLabel, subForest = Node r fs:newSubForest }, bs)
+goUp (_, []) = Nothing
 
 goDownBy :: (a -> Bool) -> TreeZipper a -> Maybe (TreeZipper a)
 goDownBy f (Node { rootLabel = r, subForest = ts }, bs) =
