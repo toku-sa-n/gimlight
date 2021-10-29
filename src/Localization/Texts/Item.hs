245f988b4e13d960595395e774e902bdd6e3f1d9
Hiroki Tokunaga <tokusan441@gmail.com>
2021-10-29 20:07:04 +0900

    refactor: add `Item.hs`

---
+++ src/Localization/Texts/Item.hs
@@ -0,0 +1,10 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Localization.Texts.Item
+    ( herb
+    ) where
+
+import           Localization (MultilingualText, multilingualText)
+
+herb :: MultilingualText
+herb = multilingualText "Herb" "薬草"
