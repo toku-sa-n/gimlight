fa96b67cdbf2d50d09fbd203079b3457d3d9ccd7
Hiroki Tokunaga <tokusan441@gmail.com>
2021-11-07 13:39:24 +0900

    chore: export `(Q|q)uestCollection`

--- src/Quest.hs
+++ src/Quest.hs
@@ -1,14 +1,19 @@
 {-# LANGUAGE DeriveGeneric #-}
 
 module Quest
-    (
+    ( QuestCollection
+    , questCollection
     ) where
 
 import           GHC.Generics   (Generic)
 import           Quest.KillBats (KillBats)
+import qualified Quest.KillBats as KillBats
 
 newtype QuestCollection =
     QuestCollection
         { killBats :: KillBats
         }
     deriving (Show, Ord, Eq, Generic)
+
+questCollection :: QuestCollection
+questCollection = QuestCollection {killBats = KillBats.killBats}
