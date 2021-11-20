1bd88a344dfb5c772c088d1a8b49811ec6e1f951
Hiroki Tokunaga <tokusan441@gmail.com>
2021-11-20 14:35:11 +0900

    fix: indexing

--- src/Dungeon/Map/JSONReader.hs
+++ src/Dungeon/Map/JSONReader.hs
@@ -20,6 +20,7 @@ parseMapFile json = do
     V2 height width <- getMapSize json
     tiles <- getTiles json
     guard $ height * width == length tiles
+    guard $ all (>= 0) tiles
     Just $ array (V2 0 0, V2 (width - 1) (height - 1)) $
         zip [V2 x y | y <- [0 .. height - 1], x <- [0 .. width - 1]] tiles
 
@@ -32,6 +33,6 @@ getMapSize json =
 getTiles :: String -> Maybe [Int]
 getTiles json =
     json ^? key "layers" . values . key "data" . _Array >>=
-    fmap (map fromIntegral) .
+    fmap (map (\x -> fromIntegral x - 1)) . -- The tile ID written in the tile JSON file starts from 0, but the ids written as `data` in the map file starts from 1 to let 0 have a meaning of "No tile". Since we will assume that all cells have a tile, we subtract 1 here.
     mapM (^? _Integer) .
     toList
