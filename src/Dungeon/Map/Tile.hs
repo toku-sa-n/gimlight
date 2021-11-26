108e859ffdc65d79216626b7a1e68570ef1397b4
Hiroki Tokunaga <tokusan441@gmail.com>
2021-11-26 14:16:47 +0900

    chore: define `Cell`

--- src/Dungeon/Map/Tile.hs
+++ src/Dungeon/Map/Tile.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric   #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Dungeon.Map.Tile
     ( TileMap
@@ -21,6 +22,8 @@ module Dungeon.Map.Tile
     , downStairs
     ) where
 
+import           Actor            (Actor)
+import           Control.Lens     (makeLenses, (&), (.~), (^.))
 import           Coord            (Coord)
 import           Data.Array       (Array, bounds, (!), (//))
 import           Data.Binary      (Binary)
@@ -32,31 +35,47 @@ import           Linear.V2        (V2 (V2))
 
 type TileId = Int
 
+data Cell =
+    Cell
+        { _tileId :: TileId
+        , actor   :: Maybe Actor
+        }
+    deriving (Show, Ord, Eq, Generic)
+
+makeLenses ''Cell
+
+instance Binary Cell
+
 newtype TileMap =
-    TileMap (Array (V2 Int) TileId)
+    TileMap (Array (V2 Int) Cell)
     deriving (Show, Ord, Eq, Generic)
 
 instance Binary TileMap
 
 tileMap :: Array (V2 Int) TileId -> TileMap
-tileMap = TileMap
+tileMap = TileMap . fmap (`Cell` Nothing)
 
 widthAndHeight :: TileMap -> V2 Int
 widthAndHeight (TileMap m) = snd (bounds m) + V2 1 1
 
 changeTileAt :: Coord -> TileId -> TileMap -> Maybe TileMap
 changeTileAt c i (TileMap m)
-    | isJust $ tileIdAt c (TileMap m) = Just $ TileMap $ m // [(c, i)]
+    | isJust $ tileIdAt c (TileMap m) = Just $ TileMap $ m // [(c, newTile)]
     | otherwise = Nothing
+  where
+    newTile = m ! c & tileId .~ i
 
 walkableMap :: TileCollection -> TileMap -> BoolMap
-walkableMap tc (TileMap m) = isWalkable . (tc !) <$> m
+walkableMap tc (TileMap m) = isWalkable . ((tc !) . (^. tileId)) <$> m
 
 transparentMap :: TileCollection -> TileMap -> BoolMap
-transparentMap tc (TileMap m) = isTransparent . (tc !) <$> m
+transparentMap tc (TileMap m) = isTransparent . ((tc !) . (^. tileId)) <$> m
 
 tileIdAt :: Coord -> TileMap -> Maybe TileId
-tileIdAt c (TileMap m)
+tileIdAt c t = (^. tileId) <$> cellAt c t
+
+cellAt :: Coord -> TileMap -> Maybe Cell
+cellAt c (TileMap m)
     | c >= lower && c <= upper = Just $ m ! c
     | otherwise = Nothing
   where
@@ -83,7 +102,7 @@ tile :: Bool -> Bool -> Tile
 tile = Tile
 
 allWallTiles :: V2 Int -> TileMap
-allWallTiles wh = TileMap $ M.generate wh (const wallTile)
+allWallTiles wh = TileMap $ M.generate wh (const (Cell wallTile Nothing))
 
 isWalkable :: Tile -> Bool
 isWalkable = walkable
