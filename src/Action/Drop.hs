7ff3532437c9f6d8e42aa5e4b5c0bf6107cb7707
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-03 15:53:37 +0900

    fix: do not panic even if there's an item

--- src/Action/Drop.hs
+++ src/Action/Drop.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Action.Drop
     ( dropAction
     ) where
@@ -6,19 +8,30 @@ import           Action               (Action, ActionResult (ActionResult),
                                        ActionStatus (Failed, Ok))
 import           Actor                (removeNthItem)
 import           Control.Monad.Writer (tell)
-import           Dungeon              (pushActor, pushItem)
+import           Data.Maybe           (isJust)
+import           Dungeon              (popItemAt, pushActor, pushItem)
 import           Item                 (Item, getName)
+import           Localization         (multilingualText)
 import qualified Localization.Texts   as T
 
 dropAction :: Int -> Action
-dropAction n position e tiles d =
-    case item of
-        Just x -> dropItem x position newActor tiles d
-        Nothing -> do
-            tell [T.whatToDrop]
-            return $ ActionResult Failed (pushActor position e d) []
+dropAction n position e tiles d
+    | itemExists = do
+        tell
+            [ multilingualText
+                  "There is already an item at your foot."
+                  "足元には既にアイテムがある。"
+            ]
+        return $ ActionResult Failed (pushActor position e d) []
+    | otherwise =
+        case item of
+            Just x -> dropItem x position newActor tiles d
+            Nothing -> do
+                tell [T.whatToDrop]
+                return $ ActionResult Failed (pushActor position e d) []
   where
     (item, newActor) = removeNthItem n e
+    itemExists = (\(x, _) -> isJust x) $ popItemAt position d
 
 dropItem :: Item -> Action
 dropItem item position actor _ dungeon = do
