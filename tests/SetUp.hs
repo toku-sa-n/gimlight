abe7fe212bc94c905da96a8fc1b50078d3c7477e
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-06 23:27:52 +0900

    test: locate a herb

--- tests/SetUp.hs
+++ tests/SetUp.hs
@@ -9,16 +9,21 @@ import           Coord            (Coord)
 import           Data.Array       (array)
 import           Data.Maybe       (fromJust)
 import           Dungeon.Map.Cell (CellMap, TileIdLayer (TileIdLayer), cellMap,
-                                   locateActorAt)
+                                   locateActorAt, locateItemAt)
 import           Dungeon.Map.Tile (TileCollection, tile)
 import           IndexGenerator   (generator)
+import           Item             (herb)
 import           Linear.V2        (V2 (V2))
 
 initCellMap :: CellMap
 initCellMap =
     fromJust $
-    locateActorAt p playerPosition $
-    cellMap $ array (V2 0 0, V2 1 0) [(V2 0 0, emptyTile), (V2 1 0, emptyTile)]
+    locateActorAt
+        p
+        playerPosition
+        (cellMap $
+         array (V2 0 0, V2 1 0) [(V2 0 0, emptyTile), (V2 1 0, emptyTile)]) >>=
+    locateItemAt herb playerPosition
   where
     p = fst $ player generator
     emptyTile = TileIdLayer Nothing Nothing
