34c246dad7500ad721e1fe4876e7aad511169668
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-06 22:58:07 +0900

    test: locate the player

--- tests/Action/WaitSpec.hs
+++ tests/Action/WaitSpec.hs
@@ -5,11 +5,7 @@ module Action.WaitSpec
 import           Action               (ActionResult (ActionResult, killed, newCellMap, status),
                                        ActionStatus (Ok))
 import           Action.Wait          (waitAction)
-import           Actor                (player)
 import           Control.Monad.Writer (writer)
-import           Data.Maybe           (fromJust)
-import           Dungeon.Map.Cell     (locateActorAt)
-import           IndexGenerator       (generator)
 import           Linear.V2            (V2 (V2))
 import           SetUp                (initCellMap, initTileCollection)
 import           Test.Hspec           (Spec, describe, it, shouldBe)
@@ -19,11 +15,9 @@ spec =
     describe "WaitAction" $
     it "returns a Ok result." $ result `shouldBe` expected
   where
-    result = waitAction playerPosition initTileCollection cellMapWithPlayer
+    result = waitAction playerPosition initTileCollection initCellMap
     expected = writer (expectedResult, expectedLog)
     expectedResult =
-        ActionResult {status = Ok, newCellMap = cellMapWithPlayer, killed = []}
+        ActionResult {status = Ok, newCellMap = initCellMap, killed = []}
     expectedLog = []
-    p = fst $ player generator
-    cellMapWithPlayer = fromJust $ locateActorAt p playerPosition initCellMap
     playerPosition = V2 0 0
--- tests/SetUp.hs
+++ tests/SetUp.hs
@@ -3,14 +3,22 @@ module SetUp
     , initTileCollection
     ) where
 
+import           Actor            (player)
 import           Data.Array       (array)
-import           Dungeon.Map.Cell (CellMap, TileIdLayer (TileIdLayer), cellMap)
+import           Data.Maybe       (fromJust)
+import           Dungeon.Map.Cell (CellMap, TileIdLayer (TileIdLayer), cellMap,
+                                   locateActorAt)
 import           Dungeon.Map.Tile (TileCollection, tile)
+import           IndexGenerator   (generator)
 import           Linear.V2        (V2 (V2))
 
 initCellMap :: CellMap
 initCellMap =
+    fromJust $
+    locateActorAt p (V2 0 0) $
     cellMap $ array (V2 0 0, V2 0 0) [(V2 0 0, TileIdLayer Nothing Nothing)]
+  where
+    p = fst $ player generator
 
 initTileCollection :: TileCollection
 initTileCollection = array (0, 0) [(0, tile True True)]
