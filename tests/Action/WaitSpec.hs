a7649ec93b7cb3bfd87a9e83b6fa4e757a379847
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-06 22:00:54 +0900

    test: add `initTileCollection`

--- tests/Action/WaitSpec.hs
+++ tests/Action/WaitSpec.hs
@@ -7,13 +7,11 @@ import           Action               (ActionResult (ActionResult, killed, newCe
 import           Action.Wait          (waitAction)
 import           Actor                (player)
 import           Control.Monad.Writer (writer)
-import           Data.Array           (array)
 import           Data.Maybe           (fromJust)
 import           Dungeon.Map.Cell     (locateActorAt)
-import           Dungeon.Map.Tile     (tile)
 import           IndexGenerator       (generator)
 import           Linear.V2            (V2 (V2))
-import           SetUp                (initCellMap)
+import           SetUp                (initCellMap, initTileCollection)
 import           Test.Hspec           (Spec, describe, it, shouldBe)
 
 spec :: Spec
@@ -21,12 +19,11 @@ spec =
     describe "WaitAction" $
     it "returns a Ok result." $ result `shouldBe` expected
   where
-    result = waitAction playerPosition tc cellMapWithPlayer
+    result = waitAction playerPosition initTileCollection cellMapWithPlayer
     expected = writer (expectedResult, expectedLog)
     expectedResult =
         ActionResult {status = Ok, newCellMap = cellMapWithPlayer, killed = []}
     expectedLog = []
     p = fst $ player generator
-    tc = array (0, 0) [(0, tile True True)]
     cellMapWithPlayer = fromJust $ locateActorAt p playerPosition initCellMap
     playerPosition = V2 0 0
--- tests/SetUp.hs
+++ tests/SetUp.hs
@@ -1,11 +1,16 @@
 module SetUp
     ( initCellMap
+    , initTileCollection
     ) where
 
 import           Data.Array       (array)
 import           Dungeon.Map.Cell (CellMap, TileIdLayer (TileIdLayer), cellMap)
+import           Dungeon.Map.Tile (TileCollection, tile)
 import           Linear.V2        (V2 (V2))
 
 initCellMap :: CellMap
 initCellMap =
     cellMap $ array (V2 0 0, V2 0 0) [(V2 0 0, TileIdLayer Nothing Nothing)]
+
+initTileCollection :: TileCollection
+initTileCollection = array (0, 0) [(0, tile True True)]
