a16ae403b3c6ab0e7bb45779bb0263b54cc83d8e
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-26 22:52:23 +0900

    refactor: define `separatedFile`

--- tests/UI/Graphics/MapTilesSpec.hs
+++ tests/UI/Graphics/MapTilesSpec.hs
@@ -18,27 +18,23 @@ testAddTileFile = do
     tileFile <- runIO $ canonicalizePath unitedImageFile
     result <-
         fmap fromJust . runIO $
-        addTileFile tileFile empty >>=
-        addTileFile "tests/images/tiles/separated_0.png" . fromJust
+        addTileFile tileFile empty >>= addTileFile (separatedFile 0) . fromJust
     expected <-
         runIO $
         insertMultipleSeparatedFiles separatedFiles unitedImageFile empty >>=
-        insertMultipleSeparatedFiles
-            ["tests/images/tiles/separated_0.png"]
-            "tests/images/tiles/separated_0.png"
+        insertMultipleSeparatedFiles [separatedFile 0] (separatedFile 0)
     describe "addTileFile" $
         it
             "separate tile images in the tile file and adds all separated images." $
         -- `Image PixelRGBA8` does not implement `Show`. This is why we do not write `expected `shouldBe` True`.
         result == expected `shouldBe` True
   where
-    separatedFiles =
-        fmap
-            (\x -> "tests/images/tiles/separated_" ++ show x ++ ".png")
-            [0 :: Int .. 5]
+    separatedFiles = fmap separatedFile [0 :: Int .. 5]
     insertMultipleSeparatedFiles fileNames insertAs m =
         foldl (\acc (k, v) -> insert k v acc) m . zip (zipWithIndex insertAs) <$>
         mapM readSingleSeparatedFile fileNames
     zipWithIndex name = zip (repeat name) [0 ..]
     readSingleSeparatedFile = fmap (convertRGBA8 . fromRight') . readImage
     unitedImageFile = "tests/images/tiles/united.png"
+    separatedFile :: Int -> FilePath
+    separatedFile n = "tests/images/tiles/separated_" ++ show n ++ ".png"
