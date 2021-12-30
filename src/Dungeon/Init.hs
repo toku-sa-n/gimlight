0b7317e5dcd58d4a9d06efc28ba7db85b8ff61a7
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-30 20:57:56 +0900

    refactor: do not use `Either`

--- src/Dungeon/Init.hs
+++ src/Dungeon/Init.hs
@@ -1,13 +1,12 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Dungeon.Init
     ( initDungeon
     ) where
 
 import           Actor                     (player)
-import           Control.Lens              ((%%~), (&))
+import           Control.Lens              ((%~), (&))
 import           Control.Monad.State       (execStateT)
-import           Data.Either.Combinators   (mapLeft, maybeToRight)
+import           Data.Either.Combinators   (fromRight)
+import           Data.Maybe                (fromMaybe)
 import           Dungeon                   (Dungeon, cellMap)
 import           Dungeon.Map.Cell          (locateActorAt, updateExploredMap,
                                             updatePlayerFov)
@@ -22,15 +21,12 @@ initDungeon ::
     -> IO (Dungeon, TileCollection, IndexGenerator)
 initDungeon tc ig = do
     (beaeve', tc', ig'') <- beaeve tc ig'
-    let d = unwrapRight $ beaeve' & cellMap %%~ initBeaeve tc'
+    let d = beaeve' & cellMap %~ initBeaeve tc'
     return (d, tc', ig'')
   where
-    unwrapRight =
-        \case
-            Right x -> x
-            Left x  -> error x
     initBeaeve tc' cm' =
-        mapLeft show (execStateT (locateActorAt tc' player' (V2 5 5)) cm') >>=
-        maybeToRight "Failed to update the player FoV." . updatePlayerFov tc' >>=
-        Right . updateExploredMap
+        updateExploredMap .
+        fromMaybe (error "Failed to update the player FoV.") .
+        updatePlayerFov tc' . fromRight (error "Failed to locate the player.") $
+        execStateT (locateActorAt tc' player' (V2 5 5)) cm'
     (player', ig') = player ig
