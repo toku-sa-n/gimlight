be5768b1db3bf06b1f78acea125ada38b33b0e19
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-30 21:20:13 +0900

    refactor: shorten a function

--- src/Dungeon/Predefined/GlobalMap.hs
+++ src/Dungeon/Predefined/GlobalMap.hs
@@ -2,12 +2,12 @@ module Dungeon.Predefined.GlobalMap
     ( globalMap
     ) where
 
+import           Control.Arrow          (Arrow (first))
 import           Dungeon                (Dungeon, dungeon)
 import           Dungeon.Identifier     (Identifier (GlobalMap))
 import           Dungeon.Map.JSONReader (readMapTileImage)
 import           Dungeon.Map.Tile       (TileCollection)
 
 globalMap :: TileCollection -> IO (Dungeon, TileCollection)
-globalMap tc = do
-    (cm, tc') <- readMapTileImage tc "maps/global_map.json"
-    return (dungeon cm GlobalMap, tc')
+globalMap tc =
+    first (`dungeon` GlobalMap) <$> readMapTileImage tc "maps/global_map.json"
