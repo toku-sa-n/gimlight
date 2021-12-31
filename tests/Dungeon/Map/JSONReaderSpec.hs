30f3a64b77420613ba5e4c861ea66cbcf0034fab
Hiroki Tokunaga <tokusan441@gmail.com>
2021-12-31 10:09:42 +0900

    refactor: integrate functions

--- tests/Dungeon/Map/JSONReaderSpec.hs
+++ tests/Dungeon/Map/JSONReaderSpec.hs
@@ -3,7 +3,9 @@ module Dungeon.Map.JSONReaderSpec
     ) where
 
 import           Data.Map               (empty)
+import           Dungeon.Map.Cell       (CellMap)
 import           Dungeon.Map.JSONReader (readMapTileImage)
+import           Dungeon.Map.Tile       (TileCollection)
 import           SetUp.MapFile          (cellMapOfSingleTileMap,
                                          rectangleButNotSquareCellMap,
                                          rectangleButNotSquareMap,
@@ -13,24 +15,22 @@ import           Test.Hspec             (Spec, describe, it, runIO, shouldBe)
 
 spec :: Spec
 spec = do
-    testReadMapTileImage
+    testSingleTileMap
     testReadRectangleButNotSquareMap
 
-testReadMapTileImage :: Spec
-testReadMapTileImage = do
-    (resultCellMap, resultTile) <- runIO $ readMapTileImage empty singleTileMap
-    expectedTile <- runIO tilesInSingleTileFile
-    describe "readMapTileImage" $ do
-        it "loads the map file" $
-            resultCellMap `shouldBe` cellMapOfSingleTileMap
-        it "loads the tile file" $ resultTile `shouldBe` expectedTile
+testSingleTileMap :: Spec
+testSingleTileMap =
+    runIO tilesInSingleTileFile >>=
+    testReadMapTileImage singleTileMap cellMapOfSingleTileMap
 
 testReadRectangleButNotSquareMap :: Spec
-testReadRectangleButNotSquareMap = do
-    (resultCellMap, resultTile) <-
-        runIO $ readMapTileImage empty rectangleButNotSquareMap
-    expectedTile <- runIO tilesInSingleTileFile
+testReadRectangleButNotSquareMap =
+    runIO tilesInSingleTileFile >>=
+    testReadMapTileImage rectangleButNotSquareMap rectangleButNotSquareCellMap
+
+testReadMapTileImage :: FilePath -> CellMap -> TileCollection -> Spec
+testReadMapTileImage path cm tc = do
+    (resultCellMap, resultTile) <- runIO $ readMapTileImage empty path
     describe "readMapTileImage" $ do
-        it "loads the map file" $
-            resultCellMap `shouldBe` rectangleButNotSquareCellMap
-        it "loads the tile file" $ resultTile `shouldBe` expectedTile
+        it "loads the map file" $ resultCellMap `shouldBe` cm
+        it "loads the tile file" $ resultTile `shouldBe` tc
