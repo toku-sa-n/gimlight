1dff3f420742b93a353beb712c2225a7d11b4fb9
Hiroki Tokunaga <tokusan441@gmail.com>
2022-01-02 10:21:44 +0900

    test: fix existing ones

--- tests/SetUp/TileFile.hs
+++ tests/SetUp/TileFile.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE TupleSections #-}
-
 module SetUp.TileFile
     ( tilesInUnitedTileFile
     , tilesInSingleTileFile
@@ -14,6 +12,7 @@ module SetUp.TileFile
 import           Codec.Picture        (Image (imageData), PixelRGBA8)
 import           Codec.Picture.Extra  (flipHorizontally, flipVertically)
 import           Data.Bits            (Bits (bit, (.|.)))
+import           Data.Foldable        (foldlM)
 import           Data.List            (transpose)
 import           Data.List.Split      (chunksOf)
 import           Data.Map             (fromList)
@@ -24,78 +23,32 @@ import           SetUp.ImageFile      (haskellTileImage, singleTileImage)
 import           UI.Draw.Config       (tileWidth)
 
 tilesInUnitedTileFile :: IO TileCollection
-tilesInUnitedTileFile =
-    fromList <$> mapM (\x -> ((unitedTileFile, x), ) <$> tileOfIndex x) [0 .. 5]
+tilesInUnitedTileFile = fromList <$> foldlM foldStep [] [0 .. 5]
   where
+    foldStep :: [(TileIdentifier, Tile)] -> Int -> IO [(TileIdentifier, Tile)]
+    foldStep acc x =
+        fmap
+            ((acc ++) . tileList unitedTileFile x (tileOfIndex x))
+            (singleTileImage x)
     tileOfIndex n
-        | n == unwalkableAndUntransparentTile =
-            tile False False <$> singleTileImage n
-        | otherwise = tile True True <$> singleTileImage n
+        | n == unwalkableAndUntransparentTile = tile False False
+        | otherwise = tile True True
     unwalkableAndUntransparentTile = 2
 
 tilesInSingleTileFile :: IO TileCollection
 tilesInSingleTileFile =
-    fmap (fromList . (: [])) ((singleTileFile, 0), ) . tile True True <$>
-    singleTileImage 0
+    fromList . tileList singleTileFile 0 (tile True True) <$> singleTileImage 0
 
 tilesInUnwalkableTileFile :: IO TileCollection
 tilesInUnwalkableTileFile =
-    fmap (fromList . (: [])) ((unwalkableTileFile, 0), ) . tile False True <$>
+    fromList . tileList unwalkableTileFile 0 (tile False True) <$>
     singleTileImage 0
 
 haskellTile :: IO TileCollection
-haskellTile = fmap (fromList . tileList) haskellTileImage
-    -- fmap (fromList . (: [])) ((haskellTilePath, 0), ) . tile True True <$>
-    -- haskellTileImage
-    -- Transformation order is important. Tiled's specification says
-    --
-    -- > When rendering an orthographic or isometric tile, the order of
-    --   operations matters. The diagonal flip is done first, followed by the
-    --   horizontal and vertical flips. The diagonal flip should flip the
-    --   bottom left and top right corners of the tile, and can be thought of
-    --   as an x/y axis swap. For hexagonal tiles, the order does not matter.
-    --
-    -- See: https://docs.mapeditor.org/en/stable/reference/global-tile-ids/#gid-tile-flipping
-  where
-    tileList :: Image PixelRGBA8 -> [(TileIdentifier, Tile)]
-    tileList img =
-        fmap (`identifierAndTileForDVH` img) diagonalVertialHorizontal
-    identifierAndTileForDVH ::
-           (Bool, Bool, Bool) -> Image PixelRGBA8 -> (TileIdentifier, Tile)
-    identifierAndTileForDVH (d, v, h) img =
-        ( (haskellTilePath, tileIdMultiplier d v h 0)
-        , tile True True (transformImage d v h img))
-    transformImage d v h =
-        (if h
-             then flipHorizontally
-             else id) .
-        (if v
-             then flipVertically
-             else id) .
-        (if d
-             then swapImageXY
-             else id)
-    swapImageXY :: Image PixelRGBA8 -> Image PixelRGBA8
-    swapImageXY img =
-        img
-            { imageData =
-                  V.fromList
-                      (concat $
-                       transpose $ chunksOf tileWidth $ V.toList $ imageData img)
-            }
-    tileIdMultiplier :: Bool -> Bool -> Bool -> Int -> Int
-    tileIdMultiplier d v h =
-        ((if d
-              then bit 29
-              else 1) .|.
-         (if v
-              then bit 30
-              else 1) .|.
-         (if h
-              then bit 31
-              else 1) .|.)
-    diagonalVertialHorizontal =
-        (,,) <$> [False, True] <*> [False, True] <*> [False, True]
+haskellTile =
+    fmap
+        (fromList . tileList haskellTilePath 0 (tile True True))
+        haskellTileImage
 
 unitedTileFile :: FilePath
 unitedTileFile = "tests/tiles/united.json"
@@ -108,3 +61,70 @@ unwalkableTileFile = "tests/tiles/unwalkable.json"
 
 haskellTilePath :: FilePath
 haskellTilePath = "tests/tiles/haskell.json"
+
+-- Transformation order is important. Tiled's specification says
+--
+-- > When rendering an orthographic or isometric tile, the order of
+--   operations matters. The diagonal flip is done first, followed by the
+--   horizontal and vertical flips. The diagonal flip should flip the
+--   bottom left and top right corners of the tile, and can be thought of
+--   as an x/y axis swap. For hexagonal tiles, the order does not matter.
+--
+-- See: https://docs.mapeditor.org/en/stable/reference/global-tile-ids/#gid-tile-flipping
+tileList ::
+       FilePath
+    -> Int
+    -> (Image PixelRGBA8 -> Tile)
+    -> Image PixelRGBA8
+    -> [(TileIdentifier, Tile)]
+tileList path idx tileGen img =
+    fmap
+        (identifierAndTileForDVH path idx tileGen img)
+        diagonalVertialHorizontal
+
+identifierAndTileForDVH ::
+       FilePath
+    -> Int
+    -> (Image PixelRGBA8 -> Tile)
+    -> Image PixelRGBA8
+    -> (Bool, Bool, Bool)
+    -> (TileIdentifier, Tile)
+identifierAndTileForDVH path idx tileGen img (d, v, h) =
+    ((path, tileIdMultiplier d v h idx), tileGen (transformImage d v h img))
+
+transformImage :: Bool -> Bool -> Bool -> Image PixelRGBA8 -> Image PixelRGBA8
+transformImage d v h =
+    (if h
+         then flipHorizontally
+         else id) .
+    (if v
+         then flipVertically
+         else id) .
+    (if d
+         then swapImageXY
+         else id)
+
+swapImageXY :: Image PixelRGBA8 -> Image PixelRGBA8
+swapImageXY img =
+    img
+        { imageData =
+              V.fromList
+                  (concat $
+                   transpose $ chunksOf tileWidth $ V.toList $ imageData img)
+        }
+
+tileIdMultiplier :: Bool -> Bool -> Bool -> Int -> Int
+tileIdMultiplier d v h =
+    ((if d
+          then bit 29
+          else 1) .|.
+     (if v
+          then bit 30
+          else 1) .|.
+     (if h
+          then bit 31
+          else 1) .|.)
+
+diagonalVertialHorizontal :: [(Bool, Bool, Bool)]
+diagonalVertialHorizontal =
+    (,,) <$> [False, True] <*> [False, True] <*> [False, True]
