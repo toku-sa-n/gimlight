5dee69e00bb56b99ba8cedc746cdf1929d7bd43b
Hiroki Tokunaga <tokusan441@gmail.com>
2022-01-14 12:01:36 +0900

    refactor: remove duplications

--- tests/Dungeon/Generate/ConfigSpec.hs
+++ tests/Dungeon/Generate/ConfigSpec.hs
@@ -18,16 +18,20 @@ testPanicIfWidthIsTooSmall :: Spec
 testPanicIfWidthIsTooSmall =
     describe "config" $
     it "panics if the given map size is too small" $
-    evaluate (config 1 1 1 1 (V2 1 1)) `shouldThrow`
+    evaluate (config 1 1 1 1 (V2 width 1)) `shouldThrow`
     errorCall
         ("Map width is expected to be larger than or equal to " ++
-         show tileColumns ++ " but the actual value is 1.")
+         show tileColumns ++ " but the actual value is " ++ show width ++ ".")
+  where
+    width = 1
 
 testPanicIfHeightIsTooSmall :: Spec
 testPanicIfHeightIsTooSmall =
     describe "config" $
     it "panics if the given map size is too small" $
-    evaluate (config 1 1 1 1 (V2 100 1)) `shouldThrow`
+    evaluate (config 1 1 1 1 (V2 100 height)) `shouldThrow`
     errorCall
         ("Map height is expected to be larger than or equal to " ++
-         show tileRows ++ " but the actual value is 1.")
+         show tileRows ++ " but the actual value is " ++ show height ++ ".")
+  where
+    height = 1
