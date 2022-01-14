db8eff4be807ad5e8541a5e31b2a2fe1ed32e808
Hiroki Tokunaga <tokusan441@gmail.com>
2022-01-14 11:56:39 +0900

    chore: check if the height or width are valid

--- src/Dungeon/Generate/Config.hs
+++ src/Dungeon/Generate/Config.hs
@@ -1,8 +1,10 @@
 module Dungeon.Generate.Config
     ( Config(..)
+    , config
     ) where
 
-import           Linear.V2 (V2)
+import           Linear.V2      (V2 (V2))
+import           UI.Draw.Config (tileColumns, tileRows)
 
 data Config =
     Config
@@ -12,3 +14,15 @@ data Config =
         , roomMaxSize :: Int
         , mapSize     :: V2 Int
         }
+
+config :: Int -> Int -> Int -> Int -> V2 Int -> Config
+config nf mr rmin rmax ms@(V2 mw mh)
+    | mw < tileColumns =
+        error $
+        "Map width is expected to be larger than or equal to " ++
+        show tileColumns ++ " but the actual value is " ++ show mw ++ "."
+    | mh < tileRows =
+        error $
+        "Map height is expected to be larger than or equal to " ++
+        show tileRows ++ " but the actual value is " ++ show mh ++ "."
+    | otherwise = Config nf mr rmin rmax ms
--- tests/Dungeon/Generate/ConfigSpec.hs
+++ tests/Dungeon/Generate/ConfigSpec.hs
@@ -2,7 +2,32 @@ module Dungeon.Generate.ConfigSpec
     ( spec
     ) where
 
-import           Test.Hspec (Spec)
+import           Control.Exception       (evaluate)
+import           Dungeon.Generate.Config (config)
+import           Linear.V2               (V2 (V2))
+import           Test.Hspec              (Spec, describe, errorCall, it,
+                                          shouldThrow)
+import           UI.Draw.Config          (tileColumns, tileRows)
 
 spec :: Spec
-spec = undefined
+spec = do
+    testPanicIfWidthIsTooSmall
+    testPanicIfHeightIsTooSmall
+
+testPanicIfWidthIsTooSmall :: Spec
+testPanicIfWidthIsTooSmall =
+    describe "config" $
+    it "panics if the given map size is too small" $
+    evaluate (config 1 1 1 1 (V2 1 1)) `shouldThrow`
+    errorCall
+        ("Map width is expected to be larger than or equal to " ++
+         show tileColumns ++ " but the actual value is 1.")
+
+testPanicIfHeightIsTooSmall :: Spec
+testPanicIfHeightIsTooSmall =
+    describe "config" $
+    it "panics if the given map size is too small" $
+    evaluate (config 1 1 1 1 (V2 100 1)) `shouldThrow`
+    errorCall
+        ("Map height is expected to be larger than or equal to " ++
+         show tileRows ++ " but the actual value is 1.")
