31a82b41f08432447af9d69341b1dcf0d1af438a
Hiroki Tokunaga <tokusan441@gmail.com>
2022-02-01 12:56:08 +0900

    refactor: replace with variables

--- tests/Gimlight/Data/EitherSpec.hs
+++ tests/Gimlight/Data/EitherSpec.hs
@@ -16,8 +16,9 @@ testExpectRight =
         it "returns the inner value if `Right` is passed." $
             expectRight msg (Right r :: Either String String) `shouldBe` r
         it "panics with the error message if `Left` is passed." $
-            evaluate (expectRight msg (Left "Pizza")) `shouldThrow`
-            errorCall "Why not sushi!: \"Pizza\""
+            evaluate (expectRight msg (Left p)) `shouldThrow`
+            errorCall (msg ++ ": " ++ show p)
   where
     r = "Sushi"
+    p = "Pizza"
     msg = "Why not sushi!"
