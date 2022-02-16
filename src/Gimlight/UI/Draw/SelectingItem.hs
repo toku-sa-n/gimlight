876edb823a770cad76fceefc75bf5bfe39588011
Hiroki Tokunaga <tokusan441@gmail.com>
2022-02-16 23:38:48 +0900

    chore: emphasize the heading

--- src/Gimlight/UI/Draw/SelectingItem.hs
+++ src/Gimlight/UI/Draw/SelectingItem.hs
@@ -4,23 +4,63 @@ module Gimlight.UI.Draw.SelectingItem
     ( drawSelectingItem
     ) where
 
+import           Control.Lens                      ((&), (.~))
 import           Gimlight.GameConfig               (GameConfig)
 import           Gimlight.GameStatus.SelectingItem (Reason (Drop, Use),
                                                     SelectingItemHandler,
+                                                    getExploringHandler,
                                                     getItems, getReason,
                                                     getSelectingIndex)
 import           Gimlight.Item                     (getName)
 import           Gimlight.Localization             (getLocalizedText)
 import qualified Gimlight.Localization.Texts       as T
+import           Gimlight.UI.Draw.Exploring        (drawExploring)
+import           Gimlight.UI.Draw.Fonts            (bold)
 import           Gimlight.UI.Draw.KeyEvent         (withKeyEvents)
 import           Gimlight.UI.Types                 (GameWidgetNode)
-import           Monomer                           (label, vstack)
+import           Monomer                           (CmbAlignCenter (alignCenter),
+                                                    CmbAlignMiddle (alignMiddle),
+                                                    CmbBgColor (bgColor),
+                                                    CmbBorderB (borderB),
+                                                    CmbHeight (height),
+                                                    CmbPadding (padding),
+                                                    CmbStyleBasic (styleBasic),
+                                                    CmbTextColor (textColor),
+                                                    CmbTextFont (textFont),
+                                                    CmbTextSize (textSize),
+                                                    CmbWidth (width),
+                                                    Color (Color), black, box_,
+                                                    filler, label, paddingV,
+                                                    vstack, white, zstack)
+import qualified Monomer.Lens                      as L
 import           TextShow                          (TextShow (showt))
 
 drawSelectingItem :: SelectingItemHandler -> GameConfig -> GameWidgetNode
-drawSelectingItem sh c = withKeyEvents $ vstack labels
+drawSelectingItem sh c =
+    withKeyEvents $
+    zstack
+        [ drawExploring eh c
+        , filler `styleBasic` [bgColor $ black & L.a .~ 0.5]
+        , box_
+              [alignMiddle, alignCenter]
+              (vstack labels `styleBasic`
+               [ width 800
+               , height 600
+               , bgColor (Color 0x0c 0x0c 0x0c 1)
+               , padding 24
+               ])
+        ]
   where
-    labels = label topLabel : map label addAsterlist
+    eh = getExploringHandler sh
+    labels =
+        (label topLabel `styleBasic`
+         [ textColor white
+         , textSize 32
+         , textFont bold
+         , borderB 1 white
+         , paddingV 4
+         ]) :
+        map label addAsterlist
     addAsterlist =
         zipWith
             (\idx x ->
