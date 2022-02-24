e88bb05bd0980754381be58caaac93d2d457388d
Hiroki Tokunaga <tokusan441@gmail.com>
2022-02-24 10:20:02 +0900

    refactor: do not use `initCellMap`
    The more we add tests, the bigger `initCellMap` is. We are no longer
    able to manage it. What we needed were functions useful to construct
    data easily.

--- tests/Gimlight/Action/PickUpSpec.hs
+++ tests/Gimlight/Action/PickUpSpec.hs
@@ -3,25 +3,31 @@ module Gimlight.Action.PickUpSpec
     ) where
 
 import           Control.Lens                ((%~), (&))
-import           Control.Monad.State         (StateT (runStateT), execStateT)
+import           Control.Monad.State         (StateT (runStateT), evalState,
+                                              execStateT)
 import           Control.Monad.Writer        (writer)
+import           Data.Array                  (array)
+import           Data.Either.Combinators     (fromRight')
 import           Data.Maybe                  (fromJust)
 import           Data.OpenUnion              (liftUnion)
 import           Gimlight.Action             (ActionResult (ActionResult, killed, newCellMap, status),
                                               ActionStatus (Failed, Ok))
 import           Gimlight.Action.PickUp      (pickUpAction)
-import           Gimlight.Actor              (inventoryItems)
+import           Gimlight.Actor              (inventoryItems, player)
 import           Gimlight.Data.Either        (expectRight)
-import           Gimlight.Dungeon.Map.Cell   (locateActorAt, removeActorAt,
+import           Gimlight.Dungeon.Map.Cell   (TileIdLayer (TileIdLayer),
+                                              cellMap, locateActorAt,
+                                              locateItemAt, removeActorAt,
                                               removeItemAt)
+import           Gimlight.IndexGenerator     (generator)
 import           Gimlight.Inventory          (addItem)
 import           Gimlight.Item               (getName)
 import           Gimlight.Item.Defined       (herb)
 import qualified Gimlight.Localization.Texts as T
 import           Gimlight.SetUp.CellMap      (initCellMap, initTileCollection,
                                               orcWithFullItemsPosition,
-                                              orcWithoutItemsPosition,
-                                              playerPosition)
+                                              orcWithoutItemsPosition)
+import           Linear                      (V2 (V2))
 import           Test.Hspec                  (Spec, it, shouldBe)
 
 spec :: Spec
@@ -35,23 +41,33 @@ testPickUpSuccess =
     it "returns a Ok result if there is an item at the actor's foot, and player's inventory is not full." $
     result `shouldBe` expected
   where
-    result = pickUpAction playerPosition initTileCollection initCellMap
+    result = pickUpAction (V2 0 0) initTileCollection cm'
     expected = writer (expectedResult, expectedLog)
     expectedResult =
         ActionResult
             {status = Ok, newCellMap = cellMapAfterPickingUp, killed = []}
     cellMapAfterPickingUp =
         expectRight "Failed to pick up." $
-        flip execStateT initCellMap $ do
-            _ <- removeItemAt playerPosition
-            _ <- removeActorAt playerPosition
-            locateActorAt initTileCollection actorWithItem playerPosition
+        flip execStateT cm' $ do
+            _ <- removeItemAt (V2 0 0)
+            _ <- removeActorAt (V2 0 0)
+            locateActorAt initTileCollection actorWithItem (V2 0 0)
     expectedLog = [T.youGotItem $ getName herb]
     actorWithItem =
         (\(x, _) -> x & inventoryItems %~ (fromJust . addItem (liftUnion herb)))
             (expectRight
                  "Failed to add an item."
-                 (flip runStateT initCellMap $ removeActorAt playerPosition))
+                 (flip runStateT cm' $ removeActorAt (V2 0 0)))
+    cm' =
+        fromRight' $
+        flip execStateT cm $ do
+            locateActorAt
+                initTileCollection
+                (evalState player generator)
+                (V2 0 0)
+            locateItemAt initTileCollection (liftUnion herb) (V2 0 0)
+    cm =
+        cellMap $ array (V2 0 0, V2 0 0) [(V2 0 0, TileIdLayer Nothing Nothing)]
 
 testPickUpVoid :: Spec
 testPickUpVoid =
