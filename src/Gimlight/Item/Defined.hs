41d3f10ec3284d026ac18b4d6f568e0ef3432379
Hiroki Tokunaga <tokusan441@gmail.com>
2022-03-05 16:57:17 +0900

    refactor: create `images/items`

--- src/Gimlight/Item/Defined.hs
+++ src/Gimlight/Item/Defined.hs
@@ -17,20 +17,20 @@ import           Gimlight.Item.Weapon        (Weapon, weapon)
 import qualified Gimlight.Localization.Texts as T
 
 herb :: Item Heal
-herb = item T.herb "images/herb.png" (heal 4) False
+herb = item T.herb "images/items/herb.png" (heal 4) False
 
 sampleBook :: Item Book
-sampleBook = item T.sampleBook "images/book.png" T.sampleBookContent True
+sampleBook = item T.sampleBook "images/items/book.png" T.sampleBookContent True
 
 -- Equipment is marked as not usable because it must be removed from the inventory once a player equips it.
 sword :: Item Weapon
-sword = item T.sword "images/sword.png" (weapon 4) False
+sword = item T.sword "images/items/sword.png" (weapon 4) False
 
 hammer :: Item Weapon
-hammer = item T.hammer "images/hammer.png" (weapon 8) False
+hammer = item T.hammer "images/items/hammer.png" (weapon 8) False
 
 woodenArmor :: Item Armor
-woodenArmor = item T.woodenArmor "images/wood.png" (armor 4) False
+woodenArmor = item T.woodenArmor "images/items/wood.png" (armor 4) False
 
 goldenArmor :: Item Armor
-goldenArmor = item T.goldenArmor "images/golden_armor.png" (armor 8) False
+goldenArmor = item T.goldenArmor "images/items/golden_armor.png" (armor 8) False
