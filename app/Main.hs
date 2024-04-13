52c1dd8213c4e79a92d9f576bd0aa9156be65497
Hiroki Tokunaga <tokusan441@gmail.com>
2024-04-13 12:25:44 +0900

    Show a simple window

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,37 @@
-{-# LANGUAGE Safe #-}
+{-# LANGUAGE DerivingStrategies       #-}
+{-# LANGUAGE OverloadedStrings        #-}
+{-# LANGUAGE StandaloneKindSignatures #-}
+{-# LANGUAGE Unsafe                   #-}
 
 module Main
   ( main
   ) where
 
-import           Lib     (someFunc)
-import           Prelude (IO)
+import           Data.Kind (Type)
+import           Monomer   (AppEventResponse, WidgetEnv, WidgetNode, label,
+                            startApp)
+import           Prelude   (Eq, IO)
+
+type GameModel :: Type
+
+data GameModel =
+  GameModel
+  deriving stock (Eq)
+
+handleEvent ::
+     WidgetEnv GameModel ()
+  -> WidgetNode GameModel ()
+  -> GameModel
+  -> ()
+  -> [AppEventResponse GameModel ()]
+handleEvent _ _ _ _ = []
+
+buildUI :: WidgetEnv GameModel () -> GameModel -> WidgetNode GameModel ()
+buildUI _ _ = label "Press q to quit"
 
 main :: IO ()
-main = someFunc
+main = startApp initialModel handleEvent buildUI config
+  where
+    initialModel = GameModel
+    config :: [a]
+    config = []
