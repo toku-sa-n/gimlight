9a5f8f9639922f4524dd6f75b54d149951b7e503
Hiroki Tokunaga <tokusan441@gmail.com>
2024-04-20 12:49:40 +0900

    Set where to output the Haskell file

--- logic-core/src/Extraction.v
+++ logic-core/src/Extraction.v
@@ -3,6 +3,8 @@ Require Coq.extraction.ExtrHaskellNatInt.
 
 From Gimlight Require Import GameModel.
 
+Set Extraction Output Directory "src".
+
 Extraction Language Haskell.
 
 Extraction "GimlightLogicCore.hs" Gimlight.GameModel.GameModel init_game_model increment get_count.
