f6cf0f09dd9395219c5684347c56b912ff8271b4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-04-20 19:03:35 +0900

    Add a build hook

--- logic-core/Setup.hs
+++ logic-core/Setup.hs
@@ -1,3 +1,26 @@
-import           Distribution.Simple
+{-# OPTIONS_GHC -Weverything -Werror -Wno-safe -Wno-unsafe -Wno-missing-safe-haskell-mode #-}
 
-main = undefined
+module Main
+  ( main
+  ) where
+
+import           Distribution.Simple (UserHooks (buildHook, cleanHook),
+                                      defaultMainWithHooks, simpleUserHooks)
+import           Prelude             (IO, undefined)
+import           System.Process      (callCommand)
+
+main :: IO ()
+main = defaultMainWithHooks hooks
+
+hooks :: UserHooks
+hooks =
+  simpleUserHooks
+    { buildHook =
+        \pkg lbi hk flags -> do
+          callCommand "coq_makefile -f _CoqProject **/*.v -o Makefile"
+          callCommand "make -j"
+          buildHook simpleUserHooks pkg lbi hk flags
+    -- We do not implement `cleanHook` because current Cabal has a bug and does
+    -- not call it.
+    -- See https://github.com/haskell/cabal/issues/6112.
+    }
