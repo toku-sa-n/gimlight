3aa9c5a0cf0d4dcefd41996db6b97d1232724d1f
Hiroki Tokunaga <tokusan441@gmail.com>
2024-04-20 23:28:16 +0900

    Create a dummy file if necessary

--- logic-core/Setup.hs
+++ logic-core/Setup.hs
@@ -4,9 +4,11 @@ module Main
   ( main
   ) where
 
-import           Distribution.Simple (UserHooks (buildHook),
+import           Control.Monad       (unless)
+import           Distribution.Simple (UserHooks (buildHook, confHook),
                                       defaultMainWithHooks, simpleUserHooks)
-import           Prelude             (IO)
+import           Prelude             (IO, writeFile, ($))
+import           System.Directory    (doesFileExist)
 import           System.Process      (callCommand)
 
 main :: IO ()
@@ -17,6 +19,7 @@ hooks =
   simpleUserHooks
     { confHook =
         \pkg flags -> do
+          createDummyFile
           callCommand "coq_makefile -f _CoqProject **/*.v -o Makefile"
           confHook simpleUserHooks pkg flags
     , buildHook =
@@ -27,3 +30,9 @@ hooks =
     -- not call it.
     -- See https://github.com/haskell/cabal/issues/6112.
     }
+
+-- We need to create a dummy file to register the module with the `.cabal` file.
+createDummyFile :: IO ()
+createDummyFile = do
+  fileExists <- doesFileExist "src/GimlightLogicCore.hs"
+  unless fileExists $ writeFile "src/GimlightLogicCore.hs" ""
