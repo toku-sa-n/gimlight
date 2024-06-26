632e69f2ef0e7bd4d3344b7bdb2fa55fa4dd9109
Hiroki Tokunaga <tokusan441@gmail.com>
2024-06-26 12:16:02 +0900

    wip

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,18 @@
-let model =
-  Gimlight.Game_model.init_game_model |> Gimlight.Game_model.increment
-  |> Gimlight.Game_model.increment |> Gimlight.Game_model.get_count
+open Bogue
 
-let () = print_endline (Z.to_string model)
+let model = ref Gimlight.Game_model.init_game_model
+
+let increment () = model := Gimlight.Game_model.increment !model
+
+let label_message n = "You pressed the Enter key " ^ string_of_int n ^ " times"
+
+let update c m =
+  m |> Gimlight.Game_model.get_count |> Z.to_int |> label_message
+  |> Widget.set_text c
+
+let () =
+  let label = Widget.label "You pressed the Enter key 0 times" in
+  let action _ = increment () ; update label !model in
+  let button = Widget.button ~action "Press me" in
+  Layout.flat_of_w ~name:"Counter App" [label; button]
+  |> Bogue.of_layout |> Bogue.run
