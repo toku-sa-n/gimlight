a1e91c37e14a66346eb2666c28afdb885c6d5246
Hiroki Tokunaga <tokusan441@gmail.com>
2024-07-11 00:19:52 +0900

    Add a comment

--- src/core/src/Model_component_position.v
+++ src/core/src/Model_component_position.v
@@ -2,6 +2,7 @@ Set Default Goal Selector "!".
 
 From Coq Require Import Numbers.BinNums.
 
+(* (0, 0) is the top left corner of the map. *)
 Record position : Type := mk_position {
   x : positive;
   y : positive;
