98f378c7393b3c4d736c974a0e5005cfdd7bf66e
Hiroki Tokunaga <tokusan441@gmail.com>
2024-07-13 10:28:46 +0900

    wip

--- src/core/src/Model_entity_generator.v
+++ src/core/src/Model_entity_generator.v
@@ -2,9 +2,10 @@ Set Default Goal Selector "!".
 
 From Coq Require Import MSets.MSetAVL.
 From Coq Require Import Structures.OrdersEx.
-From Coq Require Import Numbers.BinNums.
-
+From Coq Require Import ZArith.ZArith.
 From Gimlight Require Import Model_entity.
+
+Open Scope N_scope.
 
 Module EntitySet := Make(N_as_OT).
 
@@ -13,6 +14,29 @@ Definition entity_generator := EntitySet.t.
 Definition mk_entity_generator : entity_generator := 
   EntitySet.empty.
 
-Definition try_mk_entity (e : entity) (eg : entity_generator) : option entity_generator :=
-  if EntitySet.mem e eg then None
-  else Some (EntitySet.add e eg).
+Definition max_entities:=1_000_000_000.
+
+Definition try_mk_entity (e : entity) (eg : entity_generator) : option (entity * entity_generator) :=
+  if EntitySet.mem e eg
+    then None
+    else Some (e, EntitySet.add e eg).
+
+Fixpoint mk_entity_rec (counter : positive)(e : entity) (eg : entity_generator) {struct counter}
+  : option (entity * entity_generator) :=
+  match counter with
+  | xI q => mk_entity_rec (xO q) (N.succ e) eg
+  | xO q => mk_entity_rec q (N.succ e) eg
+  | xH => None
+  end.
+
+Fixpoint mk_entity_rec (counter : positive)(e : entity) (eg : entity_generator) {struct counter}
+  : option (entity * entity_generator) :=
+  match try_mk_entity e eg with
+  | Some (e', eg') => Some (e', eg')
+  | None => 
+      match counter with 
+      | xI q => mk_entity_rec (xO q) (N.succ e) eg
+      | xO q => mk_entity_rec q (N.succ e) eg
+      | xH => None
+      end
+  end.
