f486c09ebb640fef4176757714943ddaae2808cb
Hiroki Tokunaga <tokusan441@gmail.com>
2024-11-17 20:43:40 +0900

    refactor: export only one OCaml file
    See the code in `src/extraction/Extraction.v`.

--- src/extraction/Extraction.v
+++ src/extraction/Extraction.v
@@ -6,4 +6,12 @@ From Gimlight Require Import GameModel.
 
 Extraction Language OCaml.
 
-Separate Extraction game_model init_game_model increment get_count.
+(* 
+  Note: We use `Extraction` instead of `Separate Extraction` due to current limitations in Dune's handling of nested modules. 
+  Specifically, Dune's `coq.extraction` stanza does not support specifying modules located in subdirectories within the `extracted_modules` field. 
+  This limitation is documented in the Dune issue tracker: https://github.com/ocaml/dune/issues/10679
+
+  To maintain a manageable project structure without flattening our directory hierarchy, we opt to extract all definitions into a single OCaml file. 
+  This approach simplifies the build process and avoids potential complications associated with unsupported nested module extractions.
+*)
+Extraction "Core.ml" game_model init_game_model increment get_count.
