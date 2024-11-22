fe64c541ddcaed669646cf535954d00558ad2495
Hiroki Tokunaga <tokusan441@gmail.com>
2024-11-22 23:47:20 +0900

    wip

--- src/lib/Coquill/List.v
+++ src/lib/Coquill/List.v
@@ -6,11 +6,15 @@ Export ListNotations.
 
 Open Scope N_scope.
 
+Create HintDb list.
+
 Fixpoint length {A : Type} (l : list A) : N :=
   match l with
   | [] => 0
   | _ :: l' => N.succ (length l')
   end.
+
+Hint Unfold length : list.
 
 Lemma app_length : forall {A : Type} (l1 l2 : list A), length (l1 ++ l2) = length l1 + length l2.
 Proof.
@@ -20,8 +24,10 @@ Proof.
     intros.
     rewrite IHl1.
     rewrite N.add_succ_l.
-    reflexivity.
+    auto.
 Qed.
+
+Hint Resolve app_length : list.
 
 Fixpoint repeat_pos {A : Type} (x : A) (n : positive) : list A :=
   match n with
@@ -30,59 +36,41 @@ Fixpoint repeat_pos {A : Type} (x : A) (n : positive) : list A :=
   | xI n' => x::repeat_pos x n' ++ repeat_pos x n'
   end.
 
+Hint Unfold repeat_pos : list.
+
 Theorem repeat_pos_length : forall {A : Type} (x : A) (n : positive), length (repeat_pos x n) = Npos n.
 Proof.
-  induction n; try reflexivity.
-  - simpl.
-    rewrite app_length.
-    rewrite IHn.
-    unfold N.succ.
-    simpl.
-    f_equal.
-    apply double_succ.
-  - simpl.
-    rewrite app_length.
-    rewrite IHn.
-    simpl.
-    f_equal.
-    apply double_0.
+  induction n; try reflexivity; simpl; rewrite app_length; rewrite IHn; unfold N.succ; simpl; f_equal; auto with positive.
 Qed.
+
+Hint Resolve repeat_pos_length : list.
 
 Theorem repeat_pos_spec : forall {A : Type} (n : positive) (x y : A), In y (repeat_pos x n) -> y = x.
 Proof.
   intros.
-  induction n.
-  - simpl in H.
-    destruct H.
-    + auto.
-    + apply IHn.
-      apply in_app_or in H.
-      destruct H; auto.
-  - simpl in H.
+  induction n; simpl in H.
+  - destruct H; auto.
     apply IHn.
     apply in_app_or in H.
     destruct H; auto.
-  - simpl in H.
+  - apply IHn.
+    apply in_app_or in H.
     destruct H; auto.
+  - destruct H; auto.
     destruct H.
 Qed.
 
+Hint Resolve repeat_pos_spec : list.
+
 Theorem repeat_pos_non_empty {A : Type} (x : A) (n : positive) : repeat_pos x n <> [].
 Proof.
-  induction n.
-  - simpl.
-    intro H.
-    inversion H.
-  - simpl.
-    intro H.
-    apply app_eq_nil in H.
-    destruct H.
-    apply IHn.
-    auto.
-  - simpl.
-    intro H.
-    inversion H.
+  induction n; simpl; intros H; try inversion H.
+  apply app_eq_nil in H.
+  destruct H.
+  auto.
 Qed.
+
+Hint Resolve repeat_pos_non_empty : list.
 
 Definition repeat {A : Type} (x : A) (n : N) : list A :=
   match n with
@@ -90,12 +78,16 @@ Definition repeat {A : Type} (x : A) (n : N) : list A :=
   | Npos p => repeat_pos x p
   end.
 
+Hint Unfold repeat : list.
+
 Theorem repeat_length : forall {A : Type} (x : A) (n : N), length (repeat x n) = n.
 Proof.
   intros.
-  destruct n; try reflexivity.
+  destruct n; auto.
   apply repeat_pos_length.
 Qed.
+
+Hint Resolve repeat_length : list.
 
 Theorem repeat_spec : forall {A : Type} (x : A) (n : N) (y : A), In y (repeat x n) -> y = x.
 Proof.
--- src/lib/Coquill/PArith.v
+++ src/lib/Coquill/PArith.v
@@ -3,6 +3,8 @@ From Coq Require Export PArith.
 #[local]
 Open Scope positive_scope.
 
+Create HintDb positive.
+
 Theorem double_0 : forall n : positive, n + n = n~0.
 Proof.
   induction n; try auto.
@@ -16,15 +18,18 @@ Proof.
     auto.
 Qed.
 
+Hint Resolve double_0 : positive.
+
 Theorem double_succ : forall n : positive, Pos.succ (n + n) = n~1.
 Proof.
   induction n; try auto.
   - simpl.
     f_equal.
     rewrite Pos.add_carry_spec.
-    rewrite IHn.
-    reflexivity.
+    auto.
   - simpl.
     f_equal.
-    apply double_0.
+    auto with positive.
 Qed.
+
+Hint Resolve double_succ : positive.
