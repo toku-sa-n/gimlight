308c47c9b1a89aa51212523967b8ec006bdb4762
Hiroki Tokunaga <tokusan441@gmail.com>
2024-12-03 22:38:28 +0900

    wip

--- src/lib/Coquill/NonEmptyArray.v
+++ src/lib/Coquill/NonEmptyArray.v
@@ -3,10 +3,21 @@ Set Default Goal Selector "!".
 From Coquill Require Array.
 From Coquill Require Import List.
 From Coquill Require Import PArith.
+From Coquill Require Import NArith.
+
+Open Scope N_scope.
 
 Definition t (A : Type) (n : positive) : Type := Array.t A (Npos n).
 
-Definition make {A : Type} (n : positive) (data : Array.t A (Npos n)) : t A n := data.
+Definition make {A : Type} {n : positive} (data : Array.t A (Npos n)) : t A n := data.
 
 Program Definition repeat {A : Type} (x : A) (n : positive) : t A n :=
-  make n (Array.repeat x (Npos n)).
+  make (Array.repeat x (Npos n)).
+
+Section UpdateRange.
+  Context {A : Type}.
+  Context {n : positive}.
+
+  Program Definition update_range (arr : t A n) (r : HalfOpenRange.t) (x : A) (upper_spec : HalfOpenRange.upper r <= N.pos n) : t A n :=
+    make (Array.update_range arr r x _).
+End UpdateRange.
