f4187939875f2123602baf89d79c8d656e3f9bec
Hiroki Tokunaga <tokusan441@gmail.com>
2024-12-05 20:31:32 +0900

    wip

--- src/lib/Coquill/List.v
+++ src/lib/Coquill/List.v
@@ -1015,7 +1015,7 @@ Section MapFirstN.
       + auto.
   Qed.
 
-  Theorem nth_error_map_first_n : forall (l : list A) n idx f H, idx < N.pos n -> nth_error (map_first_n l n f H) idx = option_map f (nth_error l idx).
+  Theorem nth_error_map_first_n_eq : forall (l : list A) n idx f H, idx < N.pos n -> nth_error (map_first_n l n f H) idx = option_map f (nth_error l idx).
   Proof.
     induction l.
     - intros.
@@ -1044,7 +1044,7 @@ Section MapFirstN.
           lia.
   Qed.
 
-  Theorem nth_map_first_n : forall (l : list A) n idx f H H1 H2, idx < N.pos n -> nth (map_first_n l n f H) idx H1 = f (nth l idx H2).
+  Theorem nth_map_first_n_eq : forall (l : list A) n idx f H H1 H2, idx < N.pos n -> nth (map_first_n l n f H) idx H1 = f (nth l idx H2).
   Proof.
     intros.
     unfold nth.
@@ -1055,7 +1055,7 @@ Section MapFirstN.
     clearbody y0.
     simpl in y0.
     destruct nth_error eqn:Heq.
-    - rewrite nth_error_map_first_n in Heq.
+    - rewrite nth_error_map_first_n_eq in Heq.
       + destruct (nth_error l idx) eqn:Heq'.
         * simpl in Heq. 
           injection Heq.
@@ -1064,6 +1064,61 @@ Section MapFirstN.
         * apply nth_error_none_length in Heq'.
           lia.
       + auto.
+    - destruct (nth_error l idx) eqn:Heq'.
+      + apply nth_error_none_length in Heq.
+        lia.
+      + apply nth_error_none_length in Heq.
+        lia.
+  Qed.
+
+  Theorem nth_error_map_first_n_neq : forall (l : list A) n idx f H, idx >= N.pos n -> nth_error (map_first_n l n f H) idx = nth_error l idx.
+  Proof.
+    induction l.
+    - intros.
+      simpl in H.
+      lia.
+    - intros.
+      simpl.
+      destruct n.
+      + simpl.
+        destruct idx.
+        * lia.
+        * simpl.
+          apply IHl.
+          lia.
+      + simpl.
+        destruct idx.
+        * lia.
+        * simpl.
+          apply IHl.
+          lia.
+      + simpl.
+        destruct idx eqn:E.
+        * lia.
+        * simpl.
+          auto.
+  Qed.
+
+  Theorem nth_map_first_n_neq : forall (l : list A) n idx f H H1 H2, idx >= N.pos n -> nth (map_first_n l n f H) idx H1 = nth l idx H2.
+  Proof.
+    intros.
+    unfold nth.
+    set (nth_obligation_1 _ _ _).
+    clearbody y.
+    simpl in y.
+    set (nth_obligation_1 _ _ _).
+    clearbody y0.
+    simpl in y0.
+    destruct nth_error eqn:Heq.
+    - destruct (nth_error l idx) eqn:Heq'.
+      + apply nth_error_map_first_n_neq with (l := l) (n := n) (f := f) (H := H) in H0.
+        rewrite H0 in Heq.
+        rewrite Heq in Heq'.
+        injection Heq'.
+        intros.
+        auto.
+      + apply nth_error_none_length in Heq'.
+        lia.
     - destruct (nth_error l idx) eqn:Heq'.
       + apply nth_error_none_length in Heq.
         lia.
@@ -1131,7 +1186,7 @@ Section MapRange.
     | x :: l' =>
       match HalfOpenRange.lower r =? 0 with
       | true => map_first_n l (HalfOpenRange.length r) f _
-      | false => f x :: map_range l' (HalfOpenRange.shift_minus r 1 _) f _
+      | false => x :: map_range l' (HalfOpenRange.shift_minus r 1 _) f _
       end
     end.
   Next Obligation.
@@ -1262,7 +1317,7 @@ Section MapRange.
       clearbody l2.
       simpl in l2.
       destruct (HalfOpenRange.lower r =? 0) eqn:E.
-      + apply nth_error_map_first_n.
+      + apply nth_error_map_first_n_eq.
         simpl in H.
         unfold HalfOpenRange.contains in H0.
         destruct H0.
@@ -1311,7 +1366,7 @@ Section MapRange.
         lia.
   Qed.
 
-  Theorem map_range_map : forall (l : list A) r f H, length l = HalfOpenRange.upper r -> map_range l r f H = map f l.
+  Theorem map_range_map : forall (l : list A) r f H, HalfOpenRange.lower r = 0 -> HalfOpenRange.upper r = length l -> map_range l r f H = map f l.
   Proof.
     induction l.
     - intros.
@@ -1319,8 +1374,8 @@ Section MapRange.
       simpl in H.
       lia.
     - intros.
-      simpl.
-      set (map_first_n_obligation_2 _ _ _ _ _ _).
+      unfold map_range.
+      set (map_range_obligation_2 _ _ _ _ _ _).
       clearbody l0.
       simpl in l0.
       set (map_range_obligation_3 _ _ _ _ _ _).
@@ -1330,35 +1385,151 @@ Section MapRange.
       clearbody l2.
       simpl in l2.
       destruct (HalfOpenRange.lower r =? 0) eqn:E.
-      + destruct (HalfOpenRange.length r) eqn:E'.
-        * simpl.
-          f_equal.
-          apply map_first_n_map.
-          simpl in H0.
-          rewrite N.eqb_eq in E.
-          rewrite <- HalfOpenRange.lower_0_length_eq_upper in H0; auto.
-          rewrite E' in H0.
-          lia.
-        * f_equal.
-          simpl.
-          apply map_first_n_map.
-          simpl in H0.
-          rewrite N.eqb_eq in E.
-          rewrite <- HalfOpenRange.lower_0_length_eq_upper in H0; auto.
-          rewrite E' in H0.
-          lia.
-        * f_equal.
-          rewrite N.eqb_eq in E.
-          rewrite <- HalfOpenRange.lower_0_length_eq_upper in H0; auto.
-          rewrite E' in H0.
-          destruct l; auto.
-          simpl in H0.
-          lia.
-      + f_equal.
-        apply IHl.
-        simpl in H.
-        simpl in H0.
-        unfold HalfOpenRange.shift_minus.
+      + apply map_first_n_map.
+        rewrite N.eqb_eq in E.
         simpl.
+        rewrite HalfOpenRange.lower_0_length_eq_upper; auto.
+      + rewrite N.eqb_neq in E.
         lia.
   Qed.
+
+  Theorem nth_error_map_range_eq : forall (l : list A) r f idx H, HalfOpenRange.contains r idx -> nth_error (map_range l r f H) idx = option_map f (nth_error l idx).
+  Proof.
+    induction l.
+    - intros.
+      assert (HalfOpenRange.upper r > 0) by apply HalfOpenRange.upper_is_positive.
+      simpl in H.
+      lia.
+    - intros.
+      unfold map_range.
+      set (map_range_obligation_2 _ _ _ _ _ _).
+      clearbody l0.
+      simpl in l0.
+      set (map_range_obligation_3 _ _ _ _ _ _).
+      clearbody l1.
+      simpl in l1.
+      set (map_range_obligation_4 _ _ _ _ _ _).
+      clearbody l2.
+      simpl in l2.
+      destruct (HalfOpenRange.lower r =? 0) eqn:E.
+      + apply nth_error_map_first_n_eq.
+        simpl in H.
+        unfold HalfOpenRange.contains in H0.
+        destruct H0.
+        rewrite HalfOpenRange.lower_0_length_eq_upper; auto.
+        rewrite N.eqb_eq in E.
+        auto.
+      + rewrite N.eqb_neq in E.
+        unfold HalfOpenRange.contains in H0.
+        simpl.
+        destruct idx eqn:E'.
+        * destruct H0.
+          lia.
+        * apply IHl. 
+          simpl.
+          unfold HalfOpenRange.contains.
+          split.
+          -- simpl.
+             lia.
+          -- simpl.
+             lia.
+  Qed.
+
+  Theorem nth_map_range_eq : forall (l : list A) r f idx H H1 H2, HalfOpenRange.contains r idx -> nth (map_range l r f H) idx H1 = f (nth l idx H2).
+  Proof.
+    intros.
+    unfold nth.
+    set (nth_obligation_1 _ _ _).
+    clearbody y.
+    simpl in y.
+    set (nth_obligation_1 _ _ _).
+    clearbody y0.
+    simpl in y0.
+    destruct nth_error eqn:Heq.
+    - rewrite nth_error_map_range_eq in Heq.
+      + destruct (nth_error l idx) eqn:Heq'.
+        * simpl in Heq. 
+          injection Heq.
+          intros.
+          auto.
+        * apply nth_error_none_length in Heq'.
+          lia.
+      + auto.
+    - destruct (nth_error l idx) eqn:Heq'.
+      + apply nth_error_none_length in Heq.
+        lia.
+      + apply nth_error_none_length in Heq.
+        lia.
+  Qed.
+
+  Theorem nth_error_map_range_neq : forall (l : list A) r f idx H, ~ (HalfOpenRange.contains r idx) -> nth_error (map_range l r f H) idx = nth_error l idx.
+  Proof.
+    induction l.
+    - intros.
+      assert (HalfOpenRange.upper r > 0) by apply HalfOpenRange.upper_is_positive.
+      simpl in H.
+      lia.
+    - intros.
+      unfold map_range.
+      set (map_range_obligation_2 _ _ _ _ _ _).
+      clearbody l0.
+      simpl in l0.
+      set (map_range_obligation_3 _ _ _ _ _ _).
+      clearbody l1.
+      simpl in l1.
+      set (map_range_obligation_4 _ _ _ _ _ _).
+      clearbody l2.
+      simpl in l2.
+      destruct (HalfOpenRange.lower r =? 0) eqn:E.
+      + apply nth_error_map_first_n_neq.
+        assert (N.pos (HalfOpenRange.length r) = HalfOpenRange.upper r).
+        {
+          apply HalfOpenRange.lower_0_length_eq_upper.
+          rewrite N.eqb_eq in E.
+          auto.
+        }
+        rewrite H1.
+        apply (HalfOpenRange.lower_0_not_contained_gt r idx).
+        * rewrite N.eqb_eq in E.
+          auto.
+        * auto.
+      + simpl.
+        destruct idx eqn:E'.
+        * auto.
+        * apply IHl.
+          simpl.
+          unfold HalfOpenRange.contains.
+          intro.
+          unfold HalfOpenRange.shift_minus in H1.
+          simpl in H1.
+          unfold HalfOpenRange.contains in H0.
+          lia.
+  Qed.
+
+  Theorem nth_map_range_neq : forall (l : list A) r f idx H H1 H2, ~ (HalfOpenRange.contains r idx) -> nth (map_range l r f H) idx H1 = nth l idx H2.
+  Proof.
+    intros.
+    unfold nth.
+    set (nth_obligation_1 _ _ _).
+    clearbody y.
+    simpl in y.
+    set (nth_obligation_1 _ _ _).
+    clearbody y0.
+    simpl in y0.
+    destruct nth_error eqn:Heq.
+    - destruct (nth_error l idx) eqn:Heq'.
+      + apply nth_error_map_range_neq with (l := l) (r := r) (f := f) (H := H) in H0.
+        rewrite H0 in Heq.
+        rewrite Heq in Heq'.
+        injection Heq'.
+        intros.
+        auto.
+      + apply nth_error_none_length in Heq'.
+        lia.
+    - destruct (nth_error l idx) eqn:Heq'.
+      + apply nth_error_none_length in Heq.
+        lia.
+      + apply nth_error_none_length in Heq.
+        lia.
+  Qed.
+End MapRange.
