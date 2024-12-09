2b4f3bdd55a35f4b2f0012e81d7a783fb5a4f0c4
Hiroki Tokunaga <tokusan441@gmail.com>
2024-12-10 00:32:47 +0900

    refactor: remove lots of modules from `Coquill`

--- src/lib/Coquill/Array.v
+++ src/lib/Coquill/Array.v
@@ -2,105 +2,14 @@ Set Default Goal Selector "!".
 
 From Coq Require Import NArith.
 
-From Coquill Require Import List.
-From Coquill Require HalfOpenRange.
-
 Open Scope N_scope.
 
-Create HintDb array.
+Inductive t (A : Type) : N -> Type :=
+  | empty : t A 0
+  | cons : forall {n : N}, A -> t A n -> t A (n + 1).
 
-Record t (A : Type) (n : N) : Type := make {
-  inner_list : list A;
-  length_spec : length inner_list = n;
-}.
+Arguments empty {A}.
+Arguments cons {A n} _ _.
 
-Arguments make {A n} inner_list length_spec.
-Arguments inner_list {A n} _.
-Arguments length_spec {A n} _.
 
-Hint Constructors t : array.
 
-Definition empty (A : Type) : t A 0 := make [] eq_refl.
-
-Section Repeat.
-  Context {A : Type}.
-
-  Program Definition repeat (x : A) (n : N) : t A n :=
-    make (List.repeat x n) _.
-  Next Obligation.
-  Proof.
-    apply length_repeat.
-  Qed.
-End Repeat.
-
-Section UpdateRange.
-  Context {A : Type}.
-  Context {n : N}.
-
-  Program Definition update_range (arr : t A n) (r : HalfOpenRange.t) (x : A) (upper_spec : HalfOpenRange.upper r <= n) : t A n :=
-    make (List.update_range (inner_list arr) r x _) _.
-  Next Obligation.
-  Proof.
-    rewrite length_spec.
-    auto.
-  Qed.
-  Next Obligation.
-  Proof.
-    rewrite length_update_range.
-    apply length_spec.
-  Qed. 
-End UpdateRange.
-
-Section MapNth.
-  Context {A : Type}.
-  Context {n : N}.
-
-  Program Definition map_nth (arr : t A n) (i : N) (f : A -> A) (i_spec : i < n) : t A n :=
-    make (List.map_nth (inner_list arr) i f _) _.
-  Next Obligation.
-  Proof.
-    rewrite length_spec.
-    auto.
-  Qed.
-  Next Obligation.
-  Proof.
-    rewrite length_map_nth.
-    apply length_spec.
-  Qed.
-End MapNth.
-
-Section Update.
-  Context {A : Type}.
-  Context {n : N}.
-
-  Program Definition update (arr : t A n) (i : N) (x : A) (i_spec : i < n) : t A n :=
-    make (List.update (inner_list arr) i x _) _.
-  Next Obligation.
-  Proof.
-    rewrite length_spec.
-    auto.
-  Qed.
-  Next Obligation.
-  Proof.
-    rewrite length_update.
-    apply length_spec.
-  Qed.
-End Update.
-
-Section MapRange.
-  Context {A : Type}.
-  Context {n : N}.
-
-  Program Definition map_range (arr : t A n) (r : HalfOpenRange.t) (f : A -> A) (r_spec : HalfOpenRange.upper r <= n) : t A n :=
-    make (List.map_range (inner_list arr) r f _) _.
-  Next Obligation.
-  Proof.
-    rewrite length_spec.
-    auto.
-  Qed.
-  Next Obligation.
-  Proof.
-    rewrite length_map_range.
-    apply length_spec.
-  Qed.
-End MapRange.
