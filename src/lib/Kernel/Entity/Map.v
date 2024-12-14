a77e1c7f05d9a37cfe5b5077dc62f56452b11634
Hiroki Tokunaga <tokusan441@gmail.com>
2024-12-15 00:16:17 +0900

    wip

--- src/lib/Kernel/Entity/Map.v
+++ src/lib/Kernel/Entity/Map.v
@@ -1,5 +1,7 @@
 Set Default Goal Selector "!".
 
+From Coq Require Import FunctionalExtensionality.
+From Coq Require Import ProofIrrelevance.
 From Coq Require Import ZArith.
 From Coq Require Import Lia.
 
@@ -29,6 +31,30 @@ Program Definition build_vertical_road
   (y_spec : y_from < y_to <= N.pos height)
   (map : t width height) : t width height :=
   FixedSizeArray.map_nth x (FixedSizeArray.update_range y_from y_to false _) _ map.
+
+Program Definition build_room
+  {width height : positive}
+  (x_from x_to y_from y_to : N)
+  (x_spec : x_from < x_to <= N.pos width)
+  (y_spec : y_from < y_to <= N.pos height)
+  (map : t width height) : t width height :=
+  FixedSizeArray.map_range x_from x_to (FixedSizeArray.update_range y_from y_to false _) _ map.
+
+Theorem build_room_eq_build_horizontal_road {width height : positive} x_from x_to y H1 H2 H3 H4 (map : t width height) :
+  build_room x_from x_to y (y + 1) H1 H2 map = build_horizontal_road x_from x_to y H3 H4 map.
+Proof.
+  unfold build_room, build_horizontal_road.
+  set (build_room_obligation_1 _ _ _ _ _ _ _ _).
+  set (build_room_obligation_2 _ _ _ _ _ _ _ _).
+  set (build_horizontal_road_obligation_1 _ _ _ _ _ _ _).
+  set (build_horizontal_road_obligation_2 _ _ _ _).
+  clearbody a a0 l a1.
+  f_equal.
+  - apply functional_extensionality.
+    intros.
+    apply update_range_eq_update_nth.
+  - apply proof_irrelevance.
+Qed.
 
 Program Definition initial_map : t 80 50 :=
   build_vertical_road 40 10 30 _ _ (build_horizontal_road 10 50 25 _ _ (all_wall_map 80 50)).
