185cd828a06408986d219f473b6a2de9d14f4a02
Hiroki Tokunaga <tokusan441@gmail.com>
2024-12-15 11:42:21 +0900

    wip

--- src/lib/Kernel/Entity/Map/Connection.v
+++ src/lib/Kernel/Entity/Map/Connection.v
@@ -2,41 +2,31 @@ Set Default Goal Selector "!".
 
 From Coq Require Import NArith.
 
+From Entity Require Import Coord.
 From Entity Require Import Map.
 
-Definition in_bounds {width height} (map : t width height) (x y : N) : Prop :=
-  (x < N.pos width) /\ (y < N.pos height).
+Definition in_bounds {width height} (map : t width height) (p : Coord.t) : Prop :=
+  match p with
+  | (x, y) => x < N.pos width /\ y < N.pos height
+  end.
 
-Definition adjacent (x1 y1 x2 y2 : N) : Prop :=
-  (x1 = x2 /\ y1 = y2 + 1) \/
-  (x1 = x2 /\ y1 = y2 - 1) \/
-  (x1 = x2 + 1 /\ y1 = y2) \/
-  (x1 = x2 - 1 /\ y1 = y2).
 
-Program Definition can_move
-  {width height}
-  (map : t width height)
-  (sx sy dx dy : N) 
-  (H1 : in_bounds map sx sy)
-  (H2 : in_bounds map dx dy) : Prop :=
-  adjacent sx sy dx dy /\ get_at dx dy _ _ map = false.
-Next Obligation.
-  unfold in_bounds in *.
-  now destruct H2.
-Qed.
-Next Obligation.
-  unfold in_bounds in *.
-  now destruct H1.
-Qed.
+Definition adjacent (p1 p2 : Coord.t) : Prop :=
+  match p1, p2 with
+  | (x1, y1), (x2, y2) =>
+    (x1 = x2 /\ y1 = y2 + 1) \/
+    (x1 = x2 /\ y1 = y2 - 1) \/
+    (x1 = x2 + 1 /\ y1 = y2) \/
+    (x1 = x2 - 1 /\ y1 = y2)
+  end.
 
-Inductive reachable
-  {width height}
-  (map : t width height)
-  : N -> N -> N -> N -> Prop :=
-  | reachable_refl : forall x y, in_bounds map x y -> reachable map x y x y
-  | reachable_step :
-      forall x y x' y' x'' y'' H1 H2,
-      in_bounds map x y ->
-      can_move map x y x' y' H1 H2 ->
-      reachable map x' y' x'' y'' ->
-      reachable map x y x'' y''.
+Definition can_move {width height} (map : t width height) (src dst : Coord.t) H1 H2 : Prop :=
+  adjacent src dst /\ get_at dst H1 H2 map = false.
+
+Inductive reachable {width height} (map : t width height) : Coord.t -> Coord.t -> Prop :=
+  | reachable_refl : forall p (H1 : in_bounds map p), reachable map p p
+  | reachable_move :
+      forall src dst mid H1 H2,
+        reachable map src mid ->
+        can_move map mid dst H1 H2 ->
+        reachable map src dst.
