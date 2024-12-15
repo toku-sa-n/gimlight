5f76916acd89dfee7eaaaac0d1239b0ef917abfb
Hiroki Tokunaga <tokusan441@gmail.com>
2024-12-15 10:12:53 +0900

    feat: build a room in a map (#137)

--- src/lib/Kernel/Entity/Map.v
+++ src/lib/Kernel/Entity/Map.v
@@ -1,6 +1,7 @@
 Set Default Goal Selector "!".
 
 From Coq Require Import FunctionalExtensionality.
+From Coq Require Import ProofIrrelevance.
 From Coq Require Import ZArith.
 From Coq Require Import Lia.
 
@@ -47,8 +48,58 @@ Proof.
   now erewrite update_range_eq_update_nth.
 Qed.
 
+Program Definition build_room
+  {width height : positive}
+  (x_from x_to y_from y_to : N)
+  (x_spec : x_from < x_to <= N.pos width)
+  (y_spec : y_from < y_to <= N.pos height)
+  (map : t width height) : t width height :=
+  FixedSizeArray.map_range x_from x_to (FixedSizeArray.update_range y_from y_to false _) _ map.
+
+Theorem build_room_eq_build_horizontal_road {width height : positive} x_from x_to y H1 H2 H3 H4 (map : t width height) :
+  build_room x_from x_to y (y + 1) H1 H2 map = build_horizontal_road x_from x_to y H3 H4 map.
+Proof.
+  unfold build_room, build_horizontal_road.
+  set (build_room_obligation_1 _ _ _ _ _ _ _ _).
+  set (build_room_obligation_2 _ _ _ _ _ _ _ _).
+  set (build_horizontal_road_obligation_1 _ _ _ _ _ _ _).
+  set (build_horizontal_road_obligation_2 _ _ _ _).
+  clearbody a a0 l a1.
+  f_equal.
+  - apply functional_extensionality.
+    intros.
+    apply update_range_eq_update_nth.
+  - apply proof_irrelevance.
+Qed.
+
+Theorem build_room_eq_build_vertical_road {width height : positive} x y_from y_to H1 H2 H3 H4 (map : t width height) :
+  build_room x (x + 1) y_from y_to H1 H2 map = build_vertical_road x y_from y_to H3 H4 map.
+Proof.
+  unfold build_room, build_vertical_road.
+  set (build_room_obligation_1 _ _ _ _ _ _ _ _).
+  set (build_room_obligation_2 _ _ _ _ _ _ _ _).
+  set (build_vertical_road_obligation_1 _ _ _ _).
+  set (build_vertical_road_obligation_2 _ _ _ _ _ _ _).
+  clearbody a a0 l a1.
+  erewrite map_range_eq_map_nth.
+  f_equal.
+  apply functional_extensionality.
+  intros.
+  assert (a = a1) by apply proof_irrelevance.
+  now rewrite H.
+Qed.
+
 Program Definition initial_map : t 80 50 :=
-  build_vertical_road 40 10 30 _ _ (build_horizontal_road 10 50 25 _ _ (all_wall_map 80 50)).
+  build_room 10 20 10 20 _ _
+  (build_vertical_road 40 10 30 _ _ (build_horizontal_road 10 50 25 _ _ (all_wall_map 80 50))).
+Next Obligation.
+Proof.
+  lia.
+Qed.
+Next Obligation.
+Proof.
+  lia.
+Qed.
 Next Obligation.
 Proof.
   lia.
