8fa5d5a60ab0a67308a376965f32fb16f2ad859f
Hiroki Tokunaga <tokusan441@gmail.com>
2025-01-05 16:15:36 +0900

    wip

--- src/lib/Kernel/GameLogic/Coord.v
+++ src/lib/Kernel/GameLogic/Coord.v
@@ -2,4 +2,13 @@ Set Default Goal Selector "!".
 
 From Coq Require Import NArith.
 
+Local Open Scope N_scope.
+
 Definition t := (N * N)%type.
+
+Definition adjacent (p1 p2 : Coord.t) : Prop :=
+  match p1, p2 with
+  | (x1, y1), (x2, y2) =>
+    x1 = x2 /\ y1 = y2 + 1 \/
+    x1 = x2 /\ y1 = y2 - 1 \/ x1 = x2 + 1 /\ y1 = y2 \/ x1 = x2 - 1 /\ y1 = y2
+  end.
--- src/lib/Kernel/GameLogic/Map/Connection.v
+++ src/lib/Kernel/GameLogic/Map/Connection.v
@@ -10,18 +10,8 @@ Definition in_bounds {width height} (map : t width height) (p : Coord.t) : Prop 
   | (x, y) => x < N.pos width /\ y < N.pos height
   end.
 
-
-Definition adjacent (p1 p2 : Coord.t) : Prop :=
-  match p1, p2 with
-  | (x1, y1), (x2, y2) =>
-    (x1 = x2 /\ y1 = y2 + 1) \/
-    (x1 = x2 /\ y1 = y2 - 1) \/
-    (x1 = x2 + 1 /\ y1 = y2) \/
-    (x1 = x2 - 1 /\ y1 = y2)
-  end.
-
 Definition can_move {width height} (map : t width height) (src dst : Coord.t) H1 H2 : Prop :=
-  adjacent src dst /\ get_at dst H1 H2 map = false.
+  Coord.adjacent src dst /\ get_at dst H1 H2 map = false.
 
 Inductive reachable {width height} (map : t width height) : Coord.t -> Coord.t -> Prop :=
   | reachable_refl : forall p (H1 : in_bounds map p), reachable map p p
