bd588a83dcaf876788ff9df3479f8a84bf97a71a
Hiroki Tokunaga <tokusan441@gmail.com>
2026-08-23 01:44:41 +0900

    refactor: remove redundant dimension positivity lemmas

--- packages/logic/Dimensions.v
+++ packages/logic/Dimensions.v
@@ -13,18 +13,4 @@ Definition width_nat (dimensions : t) : nat :=
 Definition height_nat (dimensions : t) : nat :=
   Pos.to_nat (height dimensions).
 
-Lemma width_positive (dimensions : t) :
-  0 < width_nat dimensions.
-Proof.
-  unfold width_nat.
-  apply Pos2Nat.is_pos.
-Qed.
-
-Lemma height_positive (dimensions : t) :
-  0 < height_nat dimensions.
-Proof.
-  unfold height_nat.
-  apply Pos2Nat.is_pos.
-Qed.
-
 End Dimensions.
--- packages/logic/Map.v
+++ packages/logic/Map.v
@@ -14,13 +14,4 @@ Definition default : t :=
   make {| Dimensions.width := Pos.of_nat 20;
           Dimensions.height := Pos.of_nat 10 |}.
 
-Lemma dimensions_positive (map : t) :
-  0 < Dimensions.width_nat (dimensions map) /\
-  0 < Dimensions.height_nat (dimensions map).
-Proof.
-  split.
-  - apply Dimensions.width_positive.
-  - apply Dimensions.height_positive.
-Qed.
-
 End Map.
